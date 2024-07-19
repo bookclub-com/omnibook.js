463fd7c70e41d6221642a5f83b7786c28e12a3b3
Cameron McCord <cameronmccord2@gmail.com>
2024-07-19 15:42:55 -0400

    enable logging

--- src/book-block.ts
+++ src/book-block.ts
@@ -241,7 +241,7 @@ export class BookBlock<T extends IBookBlock> {
 
   toDeckEditorJS(children: IRenderBlock[]): OutputBlockData[] {
     const texts = this.getRawBlockText();
-    // console.log("type", this.type, "texts", texts);
+    console.log("type", this.type, "texts", texts);
     switch (this.type) {
       case BaseBlockTypes.TEXT:
         if (texts.length === 0) return [];
